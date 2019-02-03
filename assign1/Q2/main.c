d5d74f5c17c1577f37badab3531b4372ced5419b
gnyaneekandukuri <gnyaneekandukuri97@gmail.com>
2019-02-03 10:46:23 +0530

    added 2 question

---
+++ assign1/Q2/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[15];
+    int count =0;
+    int space=0,tab =0,digit= 0,alpha =0;
+  printf ("Enter a string pf characters");
+ scanf("%[^\n]s ",a);
+ while(count <= 15){
+   
+     if(a[count] == ' '){
+         space++;
+     }
+     if(a[count] == '\t'){
+         tab++;
+     }
+     if(a[count]>='a' && a[count]<='z' || a[count]>='A' && a[count]<='Z'){
+         alpha++;
+     }
+    if(a[count]>='0' && a[count] <= '9'){
+        digit++;
+    }
+      count++;
+     
+ }
+  printf("%s \n",a);
+ printf("spaces %d : tab %d : alpha %d :digit %d",space,tab,alpha,digit);
+
+
+
+ 
+
+    return 0;
+}
